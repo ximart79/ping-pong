efc23be8c31b463c3e4fae22d1a8f35858d27485
Jon Stapleton <jstapleton@harrisonburg.k12.va.us>
2021-01-16 03:14:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Declare your variables here
+
+// Runs once before setup()
+function preload() {
+
+}
+
+// Runs once before draw()
+function setup() {
+  createCanvas(400, 400) // width, height of screen
+}
+
+
+// Runs in a loop forever
+function draw() {
+  background(200)
+}
