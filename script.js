134a3d5ac8683922b69952c66d841561ead554e6
Xander Martinez <ximart79@harrisonburg.k12.va.us>
2021-03-16 18:57:42 +0000

    movement

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 // Declare your variables here
+let x = 100
+let y = 200
+let r = 100
+let v = 200
 
+let img;
+let ball;
+let ballsprite;
 // Runs once before setup()
 function preload() {
-
+img = loadImage("cartoon.png")
+ball = loadImage("ball.png")
 }
 
 // Runs once before draw()
 function setup() {
   createCanvas(400, 400) // width, height of screen
+ballsprite = createSprite(200, 200)
+  ballsprite.addImage(ball)
+ballsprite.friction = 0
+ballsprite.setSpeed(-2,0)
 }
 
 
 // Runs in a loop forever
 function draw() {
-  background(200)
+  background(50)
 
+  if(keyIsDown(68)) {
+    y += 1
+  }
+
+  if(keyIsDown(65)) {
+    y -= 1
   
+  }
+  if(keyIsDown(38)) {
+    r += 1
+  }
+
+  if(keyIsDown(40)) {
+    r -= 1
+  
+  }
   // Draw a rectangle with rounded corners, each having a radius of 20.
-  rect(80, 100, 100, 200, 10);
-  ellipse(200, 200, 50) // draw an ellipse
+  // draw an ellipse
+  image(img, 0, r,150,150)
+  image(img, 350, y,150,150)
+  drawSprites() // draw all sprites in the game
+//ellipse(x, y, 50)
 }
+ball.bounce(img) // activate bouncing physics
+
 
